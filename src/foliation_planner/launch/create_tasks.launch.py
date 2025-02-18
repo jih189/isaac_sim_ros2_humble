bc4edd885d49e85f50393c1c78c88d2e9d157565
jiaming <jih189@ucsd.edu>
2025-02-17 21:20:40 -0800

    create tasks

---
+++ src/foliation_planner/launch/create_tasks.launch.py
@@ -0,0 +1,69 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+
+    package_name_description = 'fetch_description'
+    package_name_moveit_config = 'fetch_moveit2_config'
+    package_name_foliation_planner = 'foliation_planner'
+
+    # Set the path to different files and folders
+    pkg_share_description = FindPackageShare(package=package_name_description).find(package_name_description)
+    pkg_share_moveit_config = FindPackageShare(package=package_name_moveit_config).find(package_name_moveit_config)
+    pkg_share_foliation_planner = FindPackageShare(package=package_name_foliation_planner).find(package_name_foliation_planner)
+    
+    urdf_file_path = 'robots/fetch.urdf.xacro'
+    srdf_file_path = 'config/fetch.srdf'
+    rviz_file_path = 'rviz/fetch.rviz'
+    moveit_controllers_file_path = 'config/controllers.yaml'
+    collision_spheres_file_path = 'robots/fetch.collision_spheres.yaml'
+
+    urdf_model_path = os.path.join(pkg_share_description, urdf_file_path)
+    srdf_model_path = os.path.join(pkg_share_moveit_config, srdf_file_path)
+    rviz_config_path = os.path.join(pkg_share_foliation_planner, rviz_file_path)
+    moveit_controllers_file_path = os.path.join(pkg_share_moveit_config, moveit_controllers_file_path)
+    collision_spheres_file_path = os.path.join(pkg_share_description, collision_spheres_file_path)
+
+    moveit_config = (
+        MoveItConfigsBuilder("fetch", package_name=package_name_foliation_planner)
+        .robot_description(file_path=urdf_model_path, mappings={"use_fake_controller": "true"})
+        .robot_description_semantic(file_path=srdf_model_path)
+        .trajectory_execution(file_path=moveit_controllers_file_path)
+        .to_moveit_configs()
+    )
+
+    node_list = []
+
+    create_motion_tasks_node = Node(
+        package='foliation_planner',
+        executable='create_motion_tasks',
+        name='create_motion_tasks_node',
+        output='screen',
+        parameters=[
+            moveit_config.to_dict(),
+            {
+                "collision_spheres_file_path": collision_spheres_file_path
+            }
+        ]
+    )
+
+    # # Launch RViz
+    # rviz_node = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     name="rviz2",
+    #     output="log",
+    #     arguments=["-d", rviz_config_path],
+    #     parameters=[
+    #         moveit_config.robot_description,
+    #         moveit_config.robot_description_semantic,
+    #     ],
+    # )
+
+    node_list.append(create_motion_tasks_node)
+    # node_list.append(rviz_node)
+
+    return LaunchDescription(node_list)
---
+++ src/foliation_planner/src/create_motion_tasks.cpp
@@ -0,0 +1,236 @@
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/planning_pipeline/planning_pipeline.h>
+#include "moveit/planning_interface/planning_interface.h"
+#include "moveit/robot_state/conversions.h"
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit/robot_trajectory/robot_trajectory.h>
+
+// cudampl include
+#include <CUDAMPLib/multiply.h>
+#include <CUDAMPLib/kinematics.h>
+#include <CUDAMPLib/cost.h>
+#include <CUDAMPLib/spaces/SingleArmSpace.h>
+#include <CUDAMPLib/constraints/EnvConstraint.h>
+#include <CUDAMPLib/constraints/SelfCollisionConstraint.h>
+#include <CUDAMPLib/tasks/SingleArmTask.h>
+#include <CUDAMPLib/planners/RRG.h>
+#include <CUDAMPLib/termination/StepTermination.h>
+#include <CUDAMPLib/termination/TimeoutTermination.h>
+
+// ompl include
+#include <ompl/base/SpaceInformation.h>
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/geometric/SimpleSetup.h>
+
+#include "foliation_planner/robot_info.hpp"
+
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
+#include <filesystem>
+
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("Motion Planning Evaluation");
+
+struct MotionPlanningTask
+{
+    std::vector<std::vector<float>> obstacle_pos;
+    std::vector<float> radius;
+    std::vector<float> start_joint_values;
+    std::vector<float> goal_joint_values;
+};
+
+void prepare_obstacles(std::vector<std::vector<float>> & balls_pos, std::vector<float> & ball_radius)
+{
+    float obstacle_spheres_radius = 0.06;
+    int num_of_obstacle_spheres = 20;
+    for (int i = 0; i < num_of_obstacle_spheres; i++)
+    {
+        float x = 0.3 * ((float)rand() / RAND_MAX) + 0.3;
+        float y = 2.0 * 0.5 * ((float)rand() / RAND_MAX) - 0.5;
+        float z = 1.0 * ((float)rand() / RAND_MAX) + 0.5;
+        balls_pos.push_back({x, y, z});
+        ball_radius.push_back(obstacle_spheres_radius);
+    }
+}
+
+std::vector<MotionPlanningTask> generateMotionPlanningTasks(const moveit::core::RobotModelPtr & robot_model, const std::string & group_name, int num_tasks)
+{
+    // create planning scene
+    auto world = std::make_shared<collision_detection::World>();
+    auto planning_scene = std::make_shared<planning_scene::PlanningScene>(robot_model, world);
+
+    moveit::core::RobotStatePtr robot_state = std::make_shared<moveit::core::RobotState>(robot_model);
+    // set robot state to default state
+    robot_state->setToDefaultValues();
+    const moveit::core::JointModelGroup* joint_model_group = robot_model->getJointModelGroup(group_name);
+
+    std::vector<MotionPlanningTask> tasks;
+    for(int i = 0; i < num_tasks; i++)
+    {
+        MotionPlanningTask task;
+        // generate random obstacles
+        prepare_obstacles(task.obstacle_pos, task.radius);
+
+        // clean up the planning scene
+        planning_scene->getWorldNonConst()->clearObjects();
+
+        // add those balls to the planning scene
+        for (size_t b = 0; b < task.obstacle_pos.size(); b++)
+        {
+            Eigen::Isometry3d sphere_pose = Eigen::Isometry3d::Identity();
+            sphere_pose.translation() = Eigen::Vector3d(task.obstacle_pos[b][0], task.obstacle_pos[b][1], task.obstacle_pos[b][2]);
+            planning_scene->getWorldNonConst()->addToObject("obstacle_" + std::to_string(b), shapes::ShapeConstPtr(new shapes::Sphere(task.radius[b])), sphere_pose);
+        }
+
+        // generate start and goal states
+        std::vector<float> start_joint_values;
+        std::vector<float> goal_joint_values;
+
+        // generate random start state
+        robot_state->setToRandomPositions();
+        robot_state->update();
+
+        while (not planning_scene->isStateValid(*robot_state, group_name))
+        {
+            robot_state->setToRandomPositions();
+            robot_state->update();
+        }
+
+        std::vector<double> start_joint_values_double;
+        robot_state->copyJointGroupPositions(joint_model_group, start_joint_values_double);
+        for (size_t j = 0; j < start_joint_values_double.size(); j++)
+        {
+            start_joint_values.push_back((float)start_joint_values_double[j]);
+        }
+        task.start_joint_values = start_joint_values;
+
+        // generate random goal state
+        robot_state->setToRandomPositions();
+        robot_state->update();
+
+        while (not planning_scene->isStateValid(*robot_state, group_name))
+        {
+            robot_state->setToRandomPositions();
+            robot_state->update();
+        }
+
+        std::vector<double> goal_joint_values_double;
+        robot_state->copyJointGroupPositions(joint_model_group, goal_joint_values_double);
+        for (size_t j = 0; j < goal_joint_values_double.size(); j++)
+        {
+            goal_joint_values.push_back((float)goal_joint_values_double[j]);
+        }
+
+        task.goal_joint_values = goal_joint_values;
+
+        tasks.push_back(task);
+    }
+    return tasks;
+}
+
+
+int main(int argc, char** argv)
+{
+    // ============================ parameters =================================== //
+    std::string task_dir_path = "/home/motion_planning_tasks";
+    int num_tasks = 10;
+    const std::string GROUP_NAME = "arm";
+    // =========================================================================== //
+
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto motion_planning_evaluation_node = rclcpp::Node::make_shared("motion_planning_evaluation_node", node_options);
+
+    // print out the node name
+    RCLCPP_INFO(LOGGER, "Node name: %s", motion_planning_evaluation_node->get_name());
+
+    // Create a robot model
+    robot_model_loader::RobotModelLoaderPtr robot_model_loader(
+      new robot_model_loader::RobotModelLoader(motion_planning_evaluation_node, "robot_description"));
+    const moveit::core::RobotModelPtr& kinematic_model = robot_model_loader->getModel();
+    if (kinematic_model == nullptr)
+    {
+        RCLCPP_ERROR(motion_planning_evaluation_node->get_logger(), "Failed to load robot model");
+        return 1;
+    }
+
+    // generate motion planning tasks
+    std::vector<MotionPlanningTask> tasks = generateMotionPlanningTasks(kinematic_model, GROUP_NAME, num_tasks);
+    
+    // check if the directory exists, if so, then delete it
+    if (std::filesystem::exists(task_dir_path))
+    {
+        // delete the directory
+        std::string delete_command = "rm -rf " + task_dir_path;
+        int r1 = system(delete_command.c_str());
+        if (r1 != 0)
+        {
+            RCLCPP_ERROR(motion_planning_evaluation_node->get_logger(), "Failed to delete directory");
+            return 1;
+        }
+    }
+    // create the directory
+    std::string create_command = "mkdir -p " + task_dir_path;
+    int r2 = system(create_command.c_str());
+    if (r2 != 0)
+    {
+        RCLCPP_ERROR(motion_planning_evaluation_node->get_logger(), "Failed to create directory");
+        return 1;
+    }
+
+    // create a file containing all task files' names in the directory
+    std::string metadata_path = task_dir_path + "/metadata.txt";
+    std::ofstream metadata(metadata_path);
+
+    for (size_t i = 0; i < tasks.size(); i++)
+    {
+        auto task = tasks[i];
+        // create a file for each task
+        std::string task_file_path = task_dir_path + "/task_" + std::to_string(i) + ".yaml";
+        std::ofstream task_file(task_file_path);
+
+        // save the task to the file
+        YAML::Emitter out;
+        out << YAML::BeginMap;
+        out << YAML::Key << "Task " << i;
+        out << YAML::Value << YAML::BeginMap;
+        out << YAML::Key << "Start joint values" << YAML::Value << task.start_joint_values;
+        out << YAML::Key << "Goal joint values" << YAML::Value << task.goal_joint_values;
+        out << YAML::Key << "Obstacles" << YAML::Value << YAML::BeginSeq;
+        for (size_t j = 0; j < task.obstacle_pos.size(); j++)
+        {
+            out << task.obstacle_pos[j];
+        }
+        out << YAML::EndSeq;
+        out << YAML::Key << "Radius" << YAML::Value << task.radius;
+        out << YAML::EndMap;
+        out << YAML::EndMap;
+
+        // write to file
+        metadata << "task_" + std::to_string(i) + ".yaml" + "\n";
+        
+        // write to file
+        task_file.write(out.c_str(), out.size());
+        
+        // close the file
+        task_file.close();
+    }
+
+    // close the file
+    metadata.close();
+
+    // stop the node
+    rclcpp::shutdown();
+
+    return 0;
+}
