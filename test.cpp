c66ba0503add33e86c154247201e809af29abad6
jiaming <jih189@ucsd.edu>
2025-02-24 13:54:30 -0800

    comment time measure in RRG code

--- test.cpp
+++ test.cpp
@@ -1,66 +1,54 @@
-
-__global__ void computeCollisionCostKernel(
-    float* d_self_collision_spheres_pos_in_base_link, // num_of_configurations x num_of_self_collision_spheres x 3
-    float* d_self_collision_spheres_radius, // num_of_self_collision_spheres
-    int num_of_self_collision_spheres,
-    int num_of_configurations,
-    float* d_obstacle_sphere_pos_in_base_link, // num_of_obstacle_spheres x 3
-    float* d_obstacle_sphere_radius, // num_of_obstacle_spheres
-    int num_of_obstacle_collision_spheres,
-    float* d_cost // num_of_configurations
-)
-{
-    // Get the index of the thread
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < num_of_configurations){
-        float cost = 0.0f;
-        for (int i = 0; i < num_of_self_collision_spheres; i++){ // For each self collision sphere
-            for (int j = 0; j < num_of_obstacle_collision_spheres; j++){ // For each obstacle sphere
-
-                float diff_in_x = d_self_collision_spheres_pos_in_base_link[idx * num_of_self_collision_spheres * 3 + i * 3 + 0] - d_obstacle_sphere_pos_in_base_link[j * 3 + 0];
-                float diff_in_y = d_self_collision_spheres_pos_in_base_link[idx * num_of_self_collision_spheres * 3 + i * 3 + 1] - d_obstacle_sphere_pos_in_base_link[j * 3 + 1];
-                float diff_in_z = d_self_collision_spheres_pos_in_base_link[idx * num_of_self_collision_spheres * 3 + i * 3 + 2] - d_obstacle_sphere_pos_in_base_link[j * 3 + 2];
-
-                float distance = sqrt(diff_in_x * diff_in_x + diff_in_y * diff_in_y + diff_in_z * diff_in_z); // Euclidean distance
-                float sum_of_radius = d_self_collision_spheres_radius[i] + d_obstacle_sphere_radius[j];
-
-                // the cost the overlap of the two spheres
-                cost += fmaxf(0.0f, sum_of_radius - distance);
-            }
-        }
-        d_cost[idx] = cost;
-    }
+__global__ void kernelA() {
+    // do something
 }
 
+__global__ void kernelB() {
+    // do something
+}
+
+__global__ void kernelC() {
+    // do something
+}
+
+__global__ void kernelD() {
+    // do something
+}
+
+// base class
+class Base {
+public:
+    virtual void function() = 0;
+};
 
 
-void EnvConstraint::computeCost(BaseStatesPtr states)
-{
-    // Cast the states and space information for SingleArmSpace
-    SingleArmSpaceInfoPtr space_info = std::static_pointer_cast<SingleArmSpaceInfo>(states->getSpaceInfo());
-    SingleArmStatesPtr single_arm_states = std::static_pointer_cast<SingleArmStates>(states);
-
-    // check the cost location of this constraint
-    int constraint_index = getConstraintIndex(space_info);
-    if (constraint_index == -1){
-        // raise an error
-        printf("Constraint %s is not found in the space\n", this->constraint_name.c_str());
-        return;
+// derived class A
+class A : public Base {
+public:
+    void function() {
+        kernelA();
+        cudaDeviceSynchronize();
+        kernelC();
+        cudaDeviceSynchronize();
     }
+};
 
-    float * d_cost_of_current_constraint = &(single_arm_states->getCostsCuda()[single_arm_states->getNumOfStates() * constraint_index]);
+// derived class B
+class B : public Base {
+public:
+    void function() {
+        kernelB();
+        cudaDeviceSynchronize();
+        kernelD();
+        cudaDeviceSynchronize();
+    }
+};
 
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (single_arm_states->getNumOfStates() + threadsPerBlock - 1) / threadsPerBlock;
+int main() {
+    std::vector<Base*> objects;
+    objects.push_back(new A());
+    objects.push_back(new B());
 
-    computeCollisionCostKernel<<<blocksPerGrid, threadsPerBlock>>>(
-        single_arm_states->getSelfCollisionSpheresPosInBaseLinkCuda(), 
-        space_info->d_self_collision_spheres_radius, 
-        space_info->num_of_self_collision_spheres, 
-        single_arm_states->getNumOfStates(), 
-        d_env_collision_spheres_pos_in_base_link, 
-        d_env_collision_spheres_radius, 
-        num_of_env_collision_spheres, 
-        d_cost_of_current_constraint 
-    );
+    for (auto obj : objects) {
+        obj->function();
+    }
 }
